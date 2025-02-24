71575812832092d1fd1743f8c0dac3faddac6242
ChyunKim <chkim@realsn.com>
2025-02-24 16:45:27 +0900

    mod: colors 재세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,33 +10,33 @@ export default {
     extend: {
       colors: {
         background: 'var(--background)',
-        foreground: 'var(--foreground)'
+        foreground: 'var(--foreground)',
+        primary: {
+          '1': '#4AC63F',
+          2: '#F6C92D',
+          3: '#FD7FE9',
+          4: '#62B3FD'
+        },
+        secondary: {
+          1: '#CDEDFF',
+          2: '#FFEBDF',
+          3: '#FFD5FB',
+          4: '#D6F4BF'
+        },
+        neutrals: {
+          1: '#141416',
+          2: '#23262F',
+          3: '#353945',
+          4: '#777E90',
+          5: '#B1B5C3',
+          6: '#E6E8EC',
+          7: '#F4F5F6',
+          8: '#FCFCFD'
+        }
       },
       fontFamily: {
         'noto-sans': ['var(--noto-sans)', 'dotum', 'system-ui', 'sans-serif'],
         'do-hyeon': ['var(--do-hyeon)', 'dotum', 'system-ui', 'sans-serif']
-      },
-      primary: {
-        1: '#4AC63F',
-        2: '#F6C92D',
-        3: '#FD7FE9',
-        4: '#62B3FD'
-      },
-      secondary: {
-        1: '#CDEDFF',
-        2: '#FFEBDF',
-        3: '#FFD5FB',
-        4: '#D6F4BF'
-      },
-      neutrals: {
-        1: '#141416',
-        2: '#23262F',
-        3: '#353945',
-        4: '#777E90',
-        5: '#B1B5C3',
-        6: '#E6E8EC',
-        7: '#F4F5F6',
-        8: '#FCFCFD'
       }
     }
   },
